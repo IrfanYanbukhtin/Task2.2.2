bdc24939c0daaaca3b8d94db8892fa9acb368208
Irfan Yanbukhtin <woolfyko@gmail.com>
2022-12-29 19:22:11 +0400

    task2.2

---
+++ Task2.2.2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Task2._2._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int zeroCount = 0;
+            int tmp = n;
+            while (tmp != 0)
+            {
+                if (tmp % 10 == 0) 
+                    zeroCount++;
+                tmp /= 10;
+            }
+            Console.WriteLine($"{n}-deki sifirlarin sayi = {zeroCount}");
+        }
+    }
+}
